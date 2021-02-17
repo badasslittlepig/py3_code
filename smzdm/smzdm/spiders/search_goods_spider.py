833e44c5c2b5f803dd9798ac26251c67268e6227
王崧 <skyshappiness@gmail.com>
2021-02-17 21:43:46 +0800

    增加最低历史价格提示

--- smzdm/smzdm/spiders/search_goods_spider.py
+++ smzdm/smzdm/spiders/search_goods_spider.py
@@ -15,14 +15,14 @@ class GoodsInfoSpider(scrapy.Spider):
 
     def start_requests(self):
         url = "https://search.smzdm.com/?c=home&s={search_key}&order=time&v=b"
-        search_goods_list = MysqlService().get("blog_smzdm_search", ["goods_name","key_words"], {"status":1}, False)
+        search_goods_list = MysqlService().get("blog_smzdm_search", ["goods_name","key_words","low_price"], {"status":1}, False)
         if len(search_goods_list) > 0:
             for search_goods in search_goods_list:
                 temp_url = url.format(search_key=search_goods[0])
-                add_params = {"search_goods":search_goods[0], "key_words":search_goods[1]}
+                add_params = {"search_goods":search_goods[0], "key_words":search_goods[1],"low_price":search_goods[2]}
                 yield scrapy.Request(url=temp_url, callback=self.parse, cb_kwargs=add_params)
 
-    def parse(self, response, search_goods, key_words):
+    def parse(self, response, search_goods, key_words, low_price):
         now_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         key_words_list = {}
         if key_words != "":
@@ -55,6 +55,7 @@ class GoodsInfoSpider(scrapy.Spider):
                         goods_save_data["create_date"] = now_date
                         if exist_goods_info == None:
                             MysqlService().insert("blog_smzdm_search_trace", [goods_save_data])
+                            goods_save_data["low_price"] = low_price
                             self.noticeWechat(goods_save_data)
 
     #关键词检测
@@ -74,7 +75,8 @@ class GoodsInfoSpider(scrapy.Spider):
     @staticmethod
     def noticeWechat(goods_data):
         notice_uri = "http://test.skyshappiness.com/index.php?m=Admin&c=ApiNotice&a=noticeSth"
-        notice_content = "商品标题："+goods_data["goods_title"]+"\r\n"+"商品价格："+goods_data["goods_price"]
+        notice_content = "商品标题：" + goods_data["goods_title"] + "\r\n商品价格：" + goods_data["goods_price"]
+        notice_content = notice_content + "\r\n历史低价：" + goods_data["low_price"]/100
         notice_data = {"notice_msg":notice_content, "notice_uri":goods_data["goods_url"]}
         http = urllib3.PoolManager()
         http.request("POST", notice_uri, fields=notice_data)
