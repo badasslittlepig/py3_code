59fbf8986a82ff866cc1c66564f2d0e207baea6a
王崧 <skyshappiness@gmail.com>
2022-06-21 14:13:07 +0800

    add filter author list

--- jinsu/jinsu/spiders/jinsu_sold.py
+++ jinsu/jinsu/spiders/jinsu_sold.py
@@ -9,8 +9,10 @@ from bs4 import BeautifulSoup
 class JinsuSoldSpider(scrapy.Spider):
     name              = 'jinsu_sold'
     allowed_domains   = ['bbs.527moto.com']
-    start_urls        = ['https://bbs.527moto.com/forum-12-1.html']
-    unneed_sold_user  = ["沪牌超市薛老板"]
+    start_urls        = ['https://bbs.527moto.com/forum.php?mod=forumdisplay&fid=12&filter=author&orderby=dateline']
+    unneed_sold_user  = ["沪牌超市薛老板", "聚友机车俱乐部", "上海满昌修理部", "禾清机车龍老板", "雷霆车行年糕", "聚摩club渣老板", "Zmotor-大卫", "上海倪氏",
+     "零柒机车_小勇", "奔达4S店", "啊电单车", "杨浦_ZMotor小小", "熊猫机9988", "口水娃", "浦江镇沪摩机车", "欣亦机车-小黑皮", "杨浦摩驿机车", "聚摩CLUB小宁-Inking", "景泰车行白老板",
+     "小谢", "上海苑乐", "闵行聚友二手车", "上海驾驭机车", "聚摩club-77", "雷霆车行牛老板", "上海ZMOTOR烨", "擎天机车", ""]
     wechat_notice_uri = "http://test.skyshappiness.com/index.php?m=Admin&c=ApiNotice&a=noticeSth"
     grab_notice_uri   = "http://test.skyshappiness.com/index.php?m=Open&c=Grab&a=grabUriAdd"
     request_pool      = urllib3.PoolManager()
@@ -27,7 +29,7 @@ class JinsuSoldSpider(scrapy.Spider):
             if sold_user in self.unneed_sold_user :
                 continue
             sold_uri = host_info.scheme + "://" + host_info.netloc + "/" + sold_uri_detail
-            post_data = { "acticle_title": sold_title, "grab_uri" : sold_uri, "is_grabed" : 1 }
+            post_data = { "acticle_title": sold_title, "grab_uri" : sold_uri, "is_grabed" : 1, "author": sold_user}
             notice_grab_uri_result = self.request_pool.request("POST", self.grab_notice_uri, post_data)
             return_str = notice_grab_uri_result.data.decode("UTF-8")
             return_data = json.loads(return_str)
