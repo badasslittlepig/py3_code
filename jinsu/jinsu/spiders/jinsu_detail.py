0052b60a0d2c2de9b3aabba254dd51cfb2573113
王崧 <wangsong@wangsongdeMacBook-Pro.local>
2022-10-25 14:12:30 +0800

    详情页面抓取,通知帖子有新的回复

--- jinsu/jinsu/spiders/jinsu_detail.py
+++ jinsu/jinsu/spiders/jinsu_detail.py
@@ -6,38 +6,32 @@ import urllib3
 from bs4 import BeautifulSoup
 
 
-class JinsuSoldSpider(scrapy.Spider):
-    name                = 'jinsu_detail'
-    allowed_domains     = ['bbs.527moto.com']
-    request_pool        = urllib3.PoolManager()
-    get_grab_uri        = "https://www.skyshappiness.com/index.php?m=Open&c=BbsDetail&a=getList"
-    start_urls_response = request_pool.request("POST", get_grab_uri)
-    return_str = start_urls_response.data.decode("UTF-8")
-    print(return_str)
+class JinsuDetailSpider(scrapy.Spider):
+    name              = 'jinsu_detail'
+    allowed_domains   = ['bbs.527moto.com']
+    request_pool      = urllib3.PoolManager()
+    get_grab_uri      = "https://www.skyshappiness.com/index.php?m=Open&c=BbsDetail&a=getList"
+    save_data_uri     = "https://www.skyshappiness.com/index.php?m=Open&c=BbsDetail&a=updateInfo"
 
-    wechat_notice_uri = "http://www.skyshappiness.com/index.php?m=Admin&c=ApiNotice&a=noticeSth"
-    grab_notice_uri   = "http://www.skyshappiness.com/index.php?m=Open&c=Grab&a=grabUriAdd"
+    def start_requests(self):
+        start_urls_response = self.request_pool.request("POST", self.get_grab_uri, {})
+        start_urls_str = start_urls_response.data.decode("UTF-8")
+        start_urls = json.loads(start_urls_str)
+        if start_urls["status"] != 200:
+            print("获取帖子爬取列表失败：" + start_urls["msg"])
+        for uri_info in start_urls["data"]:
+            trans_val = {"id":uri_info["id"], "md5_uri":uri_info["md5_uri"]}
+            yield scrapy.Request(url=uri_info["grab_uri"], callback=self.parse, cb_kwargs=trans_val)
 
-    def parse(self, response):
-        print(self.start_urls_response)
-        print(11111)
-        exit(11);
-        host_info = urllib.parse.urlparse(self.start_urls[0])
+
+
+    def parse(self, response, id, md5_uri):
         body_str = response.body.decode("gbk")
         soup = BeautifulSoup(body_str, "lxml")
-        sold_list = soup.find_all("tbody", id=re.compile("normalthread_"))
-        for sold_info in sold_list:
-            sold_title      = sold_info.find("th", class_="new").find("a", class_="xst").string
-            sold_uri_detail = sold_info.find("th", class_="new").find("a", class_="xst").get("href")
-            sold_user       = sold_info.find("td", class_="by").find("cite").find("a").string
-            if sold_user in self.unneed_sold_user :
-                continue
-            sold_uri = host_info.scheme + "://" + host_info.netloc + "/" + sold_uri_detail
-            post_data = { "acticle_title": sold_title, "grab_uri" : sold_uri, "is_grabed" : 1, "author": sold_user}
-            notice_grab_uri_result = self.request_pool.request("POST", self.grab_notice_uri, post_data)
-            return_str = notice_grab_uri_result.data.decode("UTF-8")
-            return_data = json.loads(return_str)
-            if return_data["status"] == 200 :
-                sold_title = sold_title + "\r\nnlp分词结果：" + return_data["data"]["nlp_name"];
-                wechat_notice_data = {"notice_msg":sold_title, "notice_uri": sold_uri, "notice_user":"ok_Tas4xrdDs5sf_A1mrr29MTtUY,ok_Tas7S8rDGbcYce8u97I6g7HK8,ok_TaswRfYvH3dCQRgMd34JrRgTw"}
-                self.request_pool.request("POST", self.wechat_notice_uri, wechat_notice_data)
+        reply_list_dom = soup.find_all("table", class_="plhin")
+        if len(reply_list_dom) > 1:
+            reply_name = reply_list_dom[1].find_all("div", class_="authi")[0].find("a").string
+            reply_floor = reply_list_dom[1].find("td", class_="plc").find("div", class_="pi").find("a").text
+            save_data = {"id":id, "md5_uri":md5_uri, "last_reply_name":reply_name, "last_grap_detail":reply_floor}
+            self.request_pool.request("POST", self.save_data_uri, save_data)
+
